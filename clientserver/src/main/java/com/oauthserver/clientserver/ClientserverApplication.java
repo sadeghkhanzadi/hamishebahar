515a08d6c43a86a7f39ef1bcd248ec845f97e6cf
s.khanzadi <s.khanzadi@dotin.ir>
2024-04-19 17:24:00 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: First Commit
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ clientserver/src/main/java/com/oauthserver/clientserver/ClientserverApplication.java
@@ -0,0 +1,13 @@
+package com.oauthserver.clientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientserverApplication.class, args);
+	}
+
+}
---
+++ clientserver/src/test/java/com/oauthserver/clientserver/ClientserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthserver.clientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
