36d09f0a3ef0157cb6cfaee134118e9230ce99e1
esakh <esakhanzadi@gmail.com>
2024-04-29 03:02:02 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:add js code in script for movement all shapes NEW METHODS :
    Edit Method :
    
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- app.vue
+++ app.vue
@@ -9,4 +9,25 @@ useHead({
     return title ? `${title} | همیشه بهار ` : 'همیشه بهار '
   }
 })
+let axis=ref([])
+onMounted(() => {
+  const shapes = document.querySelectorAll('.shape-move')
+  shapes.forEach(
+      (item,index)=>{
+        if(Math.random()>0.5){
+          axis.value.push(1)
+        }else{
+          axis.value.push(-1)
+        }
+      }
+  )
+  const base = document.body
+  base.addEventListener('mousemove', (e) => {
+    shapes.forEach((item, index) => {
+      let finalX=(e.clientX * 30) / window.innerWidth
+      let finalY = (e.clientY* 30) / window.innerHeight
+      item.style.transform=`translate(${axis.value[index]*finalX}px , ${axis.value[index]*finalY}px)`
+    })
+  })
+})
 </script>
