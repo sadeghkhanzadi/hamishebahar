4944e3f4dbd1c7dd242458c62f5f4c62c80d66fb
esakh <esakhanzadi@gmail.com>
2024-06-10 20:18:01 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:create brown component image NEW METHODS :
    Edit Method :
    
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ components/icons/brow.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex justify-center items-center w-24 h-5">
+  <img src="~/assets/image/brow/brow.png" alt="brow icon">
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
