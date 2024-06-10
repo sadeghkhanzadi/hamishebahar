84e9fdb895747fe54798e2ec8a80bcf54bb8b031
esakh <esakhanzadi@gmail.com>
2024-06-10 20:13:43 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:fixed breadcrumb NEW METHODS :
    Edit Method :
    
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/breadcrumb/index.vue
+++ components/breadcrumb/index.vue
@@ -1,13 +1,38 @@
-<script setup lang="ts">
-
+<script setup >
+const props = defineProps(['data'])
 </script>
 
 <template>
-<div>
-  صفحه 1 / صفحه 2 / صفحه 3
-</div>
+  <div class="bread-crumb">
+    <nav class="flex  items-center ">
+      <ul class="flex items-center gap-3 " >
+        <li class="flex items-center">
+          <nuxtLink to="/">صفحه اصلی</nuxtLink>
+        </li>
+        <li v-for="(item , index ) in props.data" class="flex items-center gap-2" :key="index" >
+          <div class="divider flex items-center">
+            <i class="fa-solid fa-angle-left"/>
+          </div>
+          <nuxtLink :class="{disabled:data.length-1 == index}"  :to="data.length-1 == index ? '' :item.url">{{item.name}}</nuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.bread-crumb{
+  //background: url("@/assets/image/swiper/parellex-bg.jpg");
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+a,.divider{
+  font-size: 0.8rem ;
+}
+.disabled{
+  font-weight: 500;
+  cursor: not-allowed;
+}
 
 </style>
--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+import shapeMove from "~/composables/shapeMove.client";
+
+const breadcrumb = {
+  title:"تماس با ما",
+  breadcrumb: [
+      {
+        name: "تماس با ما",
+        url: ''
+      },
+]
+}
 const data = {
-  address: 'گرگان بلوار گلشهر خیابان پارتیا بین پارتیا  16 و 18',
+  address: ' گرگان بلوار گلشهر خیابان پارتیا بین پارتیا  16 و 18',
   email: "esakhanzadi@gmail.com",
   phone: "09118738301",
   socialMedia: {
@@ -15,79 +26,71 @@ const form = reactive({
   subject: "",
   text: ""
 })
+onMounted(()=>{
+  shapeMove()
+})
 </script>
 
 <template>
   <div>
-    <breadcrumb-header-title/>
+    <breadcrumb-header-title :data="breadcrumb"/>
     <section>
-      <div class="container mx-auto">
-        <div class="form-box  grid grid-cols-12 gap-5 w-2/3 mx-auto">
-          <div class="content col-span-5 w-full">
+      <div class="container mx-auto ">
+        <div class="form-box relative grid grid-cols-12 gap-5 mx-auto w-full px-7 md:px-0 md:w-3/4 lg:w-1/2 mb-10 lg:mb-0">
+          <div class="content col-span-12 lg:col-span-5  w-full py-10 lg:py-20 ">
             <div class="content-title"><h4>ما همیشه مشتاق شنیدن از شما هستیم !</h4></div>
-            <div class="contact-us-info">
-              <div class="address">
+            <div class="contact-us-info flex flex-col gap-3 mt-5">
+              <div class="address flex flex-col gap-2">
                 <h5>آدرس </h5>
                 <span>{{ data.address }}</span>
               </div>
-              <div class="email">
+              <div class="email flex flex-col gap-2">
                 <h5>رایانامه</h5>
                 <span>{{ data.email }}</span>
               </div>
-              <div class="phone">
+              <div class="phone flex flex-col gap-2">
                 <h5>تماس</h5>
                 <span>{{ data.phone }}</span>
               </div>
             </div>
-            <div class="social-media">
-              <ul class="flex gap-3 items-center justify-center ">
-                <li class="telegram px-2 py-4 rounded-full">
-                  <nuxtLink class="flex justify-center items-center" :to="data.socialMedia.telegram"><i
-                      class="fa-brands fa-telegram-plane fa-lg"/></nuxtLink>
-                </li>
-                <li class="instagram px-2 py-4 rounded-full">
-                  <nuxtLink class="flex justify-center items-center" :to="data.socialMedia.instagram"><i
-                      class="fa-brands fa-instagram fa-lg"/></nuxtLink>
-                </li>
-                <li class="linkedin px-2 py-4 rounded-full">
-                  <nuxtLink class="flex justify-center items-center" :to="data.socialMedia.linkedin"><i
-                      class="fa-brands fa-linkedin-in fa-lg"/></nuxtLink>
-                </li>
-              </ul>
+            <div class="mt-5">
+              <social-app :data="data.socialMedia"/>
             </div>
           </div>
-          <div class="form col-span-7 p-10 shadow">
-            <div class="form-title">
+          <div class="form col-span-12 lg:col-span-7 lg:relative top-[15%] w-full p-10 lg:px-10 lg:pt-10 lg:pb-0 ">
+            <div class="form-title flex flex-col gap-1.5 mb-3">
               <h5>در تماس باشید</h5>
               <p>برای رزرو جلسه مشاوره این فرم را پر کنید.</p>
             </div>
-            <div class="form">
+            <div>
               <form action="" @submit.prevent="handleSubmit" class="flex flex-col gap-5">
                 <div>
-                  <input type="text" class="py-1 px-2 border-b w-full outline-0" placeholder=" نام">
+                  <input type="text" class="py-1 px-2 border-b w-full outline-0" placeholder="* نام">
                 </div>
                 <div>
-                  <input type="text" class="py-1 px-2 border-b w-full outline-0" placeholder="ایمیل شما">
+                  <input type="text" class="py-1 px-2 border-b w-full outline-0 " placeholder="* ایمیل شما">
                 </div>
                 <div>
-                  <input type="text" class="py-1 px-2 border-b w-full outline-0" placeholder="موضوع">
+                  <input type="text" class="py-1 px-2 border-b w-full outline-0" placeholder="* موضوع">
                 </div>
                 <div>
-                  <textarea name="" id="" class="w-full outline-0 py-1 px-2 border-b">متن پیام</textarea>
+                  <textarea name="" id="" class="w-full outline-0 py-1 px-2 border-b h-20"
+                            placeholder="* متن پیام"></textarea>
                 </div>
                 <div class="button">
-                  <button type="submit" class="flex justify-center items-center">
-                    <span class="icon"> <i class="fa-solid fa-arrow-right-long"/></span>
+                  <button type="submit"
+                          class="flex justify-center items-center  gap-2 text-white py-1.5 px-3 w-fit rounded">
+                    <i class="fa-solid fa-arrow-right-long"/>
                     <span>ارسال پیام</span>
                   </button>
                 </div>
               </form>
             </div>
           </div>
-          <div class="shape">
-            <div class="image"><img src="~/assets/image/shape/blue-dot.png" alt="shape move"></div>
-            <div class="image"><img src="~/assets/image/shape/circle-width-shadow.png" alt="shape move"></div>
-          </div>
+                    <div class="shape hidden lg:block">
+                      <div class="image-blue shape-move"><img src="~/assets/image/shape/blue-dot.png" alt="shape move"></div>
+                      <div class="image-circle shape-move"><img src="~/assets/image/shape/circle-width-shadow.png" alt="shape move"></div>
+                    </div>
         </div>
       </div>
     </section>
@@ -103,24 +106,53 @@ const form = reactive({
 </template>
 
 <style scoped lang="scss">
-.social-media {
-  color: rgb(167, 167, 167);
+@import "assets/style/_varible.scss";
 
-  li {
-    transition: 0.3s;
-    border: 1px solid rgba(0, 0, 0, 0.1);
+.content-title {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
 
-    &:hover {
-      background: #1ab69d;
-      color: #FFFFFF;
+.contact-us-info {
+  h5 {
+    font-size: 0.8rem;
+    font-weight: 600;
+  }
+
+  span {
+    color: #808080;
+    font-size: 0.8rem;
+    line-height: 26px;
+  }
+}
+
+.form {
+  background: #FFFFFF;
+
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+
+  .form-title {
+    p {
+      font-size: 0.8rem;
+      color: #808080;
+    }
+
+    h5 {
+      font-size: 1rem;
+      font-weight: 700;
     }
   }
 }
 
 form {
   input, textarea {
+    resize: block;
     transition: 0.3s;
     border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+    &::placeholder {
+      font-size: 0.6rem;
+    }
 
     &:focus {
       border-bottom: 1px solid #1ab69d;
@@ -128,9 +160,29 @@ form {
   }
 }
 
+button {
+  font-size: 0.8rem;
+  background: $linearGradiant-normal-button;
+  transition: background 0.3s;
+
+  &:hover {
+    background: $linearGradient-button;
+  }
+}
+
 .shape {
-  img {
-    scale: 0.8;
+  .image-circle {
+    position: absolute;
+    scale: 0.7;
+    left: 0;
+    top: -5%;
+  }
+  .image-blue{
+    position: absolute;
+    scale: 0.7;
+    left: -10%;
+    top: 0;
+    z-index: -1;
   }
 }
 </style>
