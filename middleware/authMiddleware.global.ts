de67df778181415f6e7b6abdc8f78e2f63682d70
esakh <esakhanzadi@gmail.com>
2024-07-08 19:36:56 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: add middleware NEW METHODS :
    Edit Method :
    
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ middleware/authMiddleware.global.ts
@@ -0,0 +1,13 @@
+import {useToast} from "vue-toastification";
+
+export default defineNuxtRouteMiddleware((to, from) => {
+    const toast = useToast()
+    const cookie = useCookie('jwt')
+if (to.path.startsWith('/dashboard') && cookie.value){
+        return
+}
+if (!cookie.value && to.path.startsWith('/dashboard')){
+    toast.error('برای وارد شدن به پنل کاربری ابتدا باید وارد شوید.')
+    return   navigateTo({path:'/'})
+}
+})
