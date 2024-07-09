0d60b2153771ae0bab99a2bc4891d21fd9a8cc3c
esakh <esakhanzadi@gmail.com>
2024-07-09 09:30:41 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:wip create CRUD event.page in dashboard NEW METHODS :
    Edit Method :
    
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/dashboard/form/news.vue
+++ components/dashboard/form/news.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-// import VuePersianDatetimePicker from 'vue-persian-datetime-picker'
-// import datePicker from '@alireza-ab/vue-persian-datepicker'
-import datePicker, {PersianDate} from '@alireza-ab/vue-persian-datepicker'
-
+const showMedia = ref(false)
 const form = reactive({
   title: "",
   text: "",
@@ -12,35 +9,67 @@ const form = reactive({
   medias: []
 })
 const status = ref(true)
+const medias = ref([])
+function selectFile(files){
+  files.forEach(file=>{
+    if (medias.value.find(item => item.id  == file.id )){
+      return
+    }
+    else{
+      medias.value.push(file)
+    }
+  })
+
+}
+function removeMedias(index:any){
+  if (medias.value.length){
+  medias.value = medias.value.filter(item => item.id != medias.value[index].id)
+  }
+}
 </script>
 
 <template>
   <div>
-    <form action="">
-      <div class="input-group">
+    <form action="" class="flex flex-col gap-y-3">
+      <div class="input-group flex flex-col gap-2">
         <label for="title">تیتر</label>
         <input type="text" id="title">
       </div>
-      <div class="input-group">
+      <div class="input-group flex flex-col gap-2">
         <label for="text">متن</label>
         <input type="text" id="text">
       </div>
-      <div class="input-group flex gap-2">
+      <div class="input-group flex flex-col gap-2">
+        <label for="startTime">زمان شروع نمایش :</label>
+        <input type="datetime-local" multiple id="startTime">
+      </div>
+      <div class="input-group flex flex-col gap-2">
+        <label for="startTime"> زمان پایان نمایش :</label>
+        <input type="datetime-local"  id="startTime">
+      </div>
+      <div class="input-group flex flex-col gap-2 flex gap-2">
         <label for="status">وضعیت :</label>
-        <div>
+        <div class="flex items-center gap-2">
           <input type="checkbox" class="w-4 h-4" v-model="status" id="status">
           <span v-if="status"> فعال </span>
           <span v-else> غیر فعال </span>
         </div>
       </div>
-      <div class="input-group">
-        <label for="startTime">بازه زمانی نمایش :</label>
-        <input type="datetime-local" id="startTime">
-        <client-only>
-          <date-picker></date-picker>
-        </client-only>
+      <div class="input-group flex flex-col gap-2 mt-3">
+        <div class="w-full flex flex-wrap gap-2 py-2 px-3  bg-blue-400 text-white rounded hover:bg-blue-500 transition duration-200 ease-linear cursor-pointer ">
+          <button type="button" @click.prevent="showMedia=true" class="text-center w-full">
+            انتخاب عکس
+          </button>
+           </div>
+      </div>
+      <div class="flex flex-wrap gap-2">
+        <div v-if="medias.length" v-for="(item , index) in medias" :key="index" class="flex gap-2 items-center bg-blue-500 text-white text-sm cursor-pointer py-0.5 px-3 rounded-full" @click="removeMedias(index)">
+          <span >{{item.name}}</span>
+          <span class="relative top-0.5 font-bold">&times;</span>
+        </div>
       </div>
     </form>
+          <dashboard-card-select-media @close="showMedia = false" @select-file="selectFile" v-if="showMedia" />
   </div>
 </template>
 
