01debc9c06ade82a4ac5124ff9dbdefd31f3833b
esakh <esakhanzadi@gmail.com>
2024-07-21 14:21:55 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: modify style in alert component NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/dashboard/modal/alert.vue
+++ components/dashboard/modal/alert.vue
@@ -6,13 +6,13 @@ const props = defineProps(['data'])
 
 <template>
 <div class="backdrop"  @click.self="emit('result' , false)" >
-  <div class="card grid gap-10 mt-[15vh] mx-auto bg-white w-10/12 sm:w-8/12 md:w-1/2 p-5 rounded-lg shadow-lg shadow-neutral-600">
+  <div class="card grid gap-5 mt-[15vh] mb-[3vh] mx-auto bg-white w-10/12 sm:w-8/12 md:w-1/2 lg:w-1/4 p-5 rounded-lg shadow-lg shadow-neutral-600">
     <div class="card-body flex items-center gap-2">
-      <h6 class="text-xl ">آیا میخواهید <span class="text-red-500 font-bold">{{data.name}}</span> را حذف کنید ؟</h6>
+      <h6 class="text-xl ">آیا میخواهید <span class="text-red-500 font-bold" v-if="data && data.name">{{data.name}}</span> را حذف کنید ؟</h6>
     </div>
     <div class="card-body flex flex-col justify-center items-center md:flex-row w-full md:justify-start md:items-center gap-2 ">
-      <button class="bg-red-500 w-full text-white font-bold py-2 px-3 rounded shadow shadow-red-100"  @click="emit('result' , true)">بله اطمینان دارم</button>
-      <button class="bg-blue-500 w-full text-white font-bold py-2 px-3 rounded shadow shadow-blue-100 " @click="emit('result' , false)">خیر</button>
+      <button class="bg-red-500 w-full text-white  py-1 px-3 rounded shadow shadow-red-100"  @click="emit('result' , true)">بله اطمینان دارم</button>
+      <button class="bg-blue-500 w-full text-white  py-1 px-3 rounded shadow shadow-blue-100 " @click="emit('result' , false)">خیر</button>
     </div>
   </div>
 </div>
@@ -20,12 +20,17 @@ const props = defineProps(['data'])
 
 <style scoped lang="scss">
 .backdrop{
-  position: absolute;
+  position: fixed;
   z-index: 5;
   inset: 0;
   width: 100%;
   height: 100%;
   background: rgba(0,0,0,0.2);
-
+ h6{
+   font-size: var(--text-md);
+ }
+  button{
+    font-size: var(--text-md);
+  }
 }
 </style>
