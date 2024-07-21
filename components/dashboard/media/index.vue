2f2837ffcd79818598de5b91e6312777a8a48cbb
esakh <esakhanzadi@gmail.com>
2024-07-21 14:36:59 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: add base url global variable and Miniaturize icons NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/dashboard/media/index.vue
+++ components/dashboard/media/index.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
  const props = defineProps(['data'])
- const {public:{baseUrl}} = useRuntimeConfig()
+ const apiBaseUrl = useState('apiBaseUrl').value
  const emit = defineEmits(['edite-card','remove-card'])
  const {data} = props
-</script>
+</script>F
 
 <template>
   <div class="card w-full hover:cursor-pointer flex items-center overflow-hidden rounded relative" >
     <div class="card-header">
       <figure class="flex items-center justify-center">
-        <img :src="`${baseUrl}/api/v1/view/media/download/${data.pathFile}`" class="w-full h-full " :alt="data.name">
+        <img :src="`${ apiBaseUrl}/api/v1/view/media/download/${data.pathFile}`" class="w-full h-full " :alt="data.name">
       </figure>
     </div>
     <div class="card-body  absolute ">
       <div class="icon flex justify-center items-center gap-2">
-        <i class="fa-regular fa-edit fa-2xl hover:text-blue-500 transition duration-300 ease-in " @click.self="emit('edite-card' , data)"/>
-        <i class="fa-regular fa-trash-alt fa-2xl hover:text-red-500 transition duration-300 ease-in" @click="emit('remove-card' , data.id)" />
+        <i class="fa-regular fa-edit bg-blue-500 px-2 py-1.5 rounded fa-md hover:bg-blue-700 transition duration-300 ease-in " @click.self="emit('edite-card' , data)"/>
+        <i class="fa-regular fa-trash-alt bg-red-500 px-2 py-1.5 rounded fa-md hover:bg-red-700 transition duration-300 ease-in" @click="emit('remove-card' , data.id)" />
       </div>
     </div>
   </div>
@@ -43,6 +43,7 @@
     }
     img{
       transition: 0.5s ease-out;
+      font-size: var(--text-md);
     }
     &:hover{
       img{
