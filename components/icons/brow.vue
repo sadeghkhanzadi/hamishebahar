081d41141d0a7cd50a8fe9792329a0b141dd6398
esakh <esakhanzadi@gmail.com>
2024-07-21 14:23:50 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: modify width and height in responsive mode NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/icons/brow.vue
+++ components/icons/brow.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="flex justify-center items-center w-24 h-5">
+<div class="flex justify-center items-center w-16 md:w-24 h-5">
   <img src="~/assets/image/brow/brow.png" alt="brow icon">
 </div>
 </template>
