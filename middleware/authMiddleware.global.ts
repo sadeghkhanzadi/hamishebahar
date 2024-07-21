b6cc1027abf77c0ca1c42f73a4f158401439c39d
esakh <esakhanzadi@gmail.com>
2024-07-21 14:23:03 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: create middleWare for check ath for dashboard NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- middleware/authMiddleware.global.ts
+++ middleware/authMiddleware.global.ts
@@ -1,13 +1,15 @@
-import {useToast} from "vue-toastification";
+// import {useToast} from "vue-toastification";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    const toast = useToast()
+    // const toast = useToast()
     const cookie = useCookie('jwt')
 if (to.path.startsWith('/dashboard') && cookie.value){
         return
 }
 if (!cookie.value && to.path.startsWith('/dashboard')){
-    toast.error('برای وارد شدن به پنل کاربری ابتدا باید وارد شوید.')
+    if (process.client){
+    // toast.error('برای وارد شدن به پنل کاربری ابتدا باید وارد شوید.')
+    }
     return   navigateTo({path:'/'})
 }
 })
