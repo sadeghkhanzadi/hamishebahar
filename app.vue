4b2c4ce477a225b6de9787a9279d3e95ad748780
esakh <esakhanzadi@gmail.com>
2024-07-21 14:48:51 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:  create variables for cookie and baseUrl NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- app.vue
+++ app.vue
@@ -4,12 +4,14 @@
     </NuxtLayout>
 </template>
 <script setup>
+const cookie = useCookie('jwt')
+useState('cookie' ,()=> cookie.value)
+useState('apiBaseUrl' ,()=> 'https://hamishebahar-service.liara.run')
 useHead({
   titleTemplate:(title)=>{
     return title ? `${title} | همیشه بهار ` : 'همیشه بهار '
   }
 })
-
 </script>
 <style>
  .father .router-link-exact-active{
