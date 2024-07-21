45bf0f901b810294df7a2820428fcd678c8ff4ea
esakh <esakhanzadi@gmail.com>
2024-07-21 15:00:10 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: add baseUrl global variable in and add newAction for separate query request and primary request NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- store/dashboard.js
+++ store/dashboard.js
@@ -1,27 +1,48 @@
 import {defineStore} from "pinia";
 const cookie = useCookie('jwt')
-const {public:{baseUrl}} = useRuntimeConfig()
+const apiBaseUrl = useState('apiBaseUrl').value
 export const useDashboardMedia = defineStore('useDashboardData',{
     state(){
         return{
-            dashboard:[]
+            dashboard:[],
+            media:[],
         }
     },
     getters:{
         getDashboardMedia(state){
             return state.dashboard
+        },
+        getMedia(state){
+            return state.media
         }
     },
     actions:{
-        async fetchDashboardData(){
-            const {data , status , error} = await useFetch(`${baseUrl}/api/v1/media`,{
+        async fetchDashboardData(queryStatus){
+            const {data , status , error} = await useFetch(`${apiBaseUrl}/api/v1/media`,{
                 headers:{
                     Authorization:`${cookie.value}`
+                },
+                query:{
+                    status:queryStatus
                 }
             })
             if (status.value === 'success'){
                 console.log(data.value)
                 this.dashboard = data.value.result.content
+            }
+            if (error.value){
+                console.log(error.value)
+            }
+        },
+        async fetchDashboardMediaData(){
+            const {data , status , error} = await useFetch(`${apiBaseUrl}/api/v1/media`,{
+                headers:{
+                    Authorization:`${cookie.value}`
+                },
+            })
+            if (status.value === 'success'){
+                console.log(data.value)
+                this.media = data.value.result.content
             }
             if (error.value){
                 console.log(error.value)
