a7e7d5938a43b61b5a4b25d43901bb169384f275
esakh <esakhanzadi@gmail.com>
2024-07-21 14:50:39 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT: create and install  Vue3PersianDatetimePicker NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ plugins/datePicker.client.js
@@ -0,0 +1,10 @@
+import Vue3PersianDatetimePicker from 'vue3-persian-datetime-picker'
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(Vue3PersianDatetimePicker, {
+        name: 'date-picker',
+        props: {
+            format: "YYYY-MM-DD",
+            displayFormat:"YYYY-MM-DD"
+        }
+    })
+})
