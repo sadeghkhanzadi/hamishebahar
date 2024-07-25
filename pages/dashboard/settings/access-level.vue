d91d33ab232db7bd907943ad09345c0c18d3b267
esakh <esakhanzadi@gmail.com>
2024-07-25 13:03:59 +0330

    USER_STORY: 1 TASK_NUMBER: 1 BUG_NUMBER: 0 COMMENT:create access-level.vue dashboard page NEW METHODS : Edit Method :
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ pages/dashboard/settings/access-level.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const showNewForm = ref(true)
+const breadcrumb = [
+  {
+    name:"سطوح دسترسی"
+  }
+]
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <header class="flex justify-between items-center ">
+      <div>
+        <dashboard-breadcrumb :data="breadcrumb"/>
+      </div>
+      <div class="flex gap-5">
+        <button class="remove-key flex items-center justify-center bg-red-500 py-2 px-5 rounded-lg text-white gap-2" @click="">حذف کلید  <i class="fa-solid fa-trash-alt"/></button>
+        <button class="write-key flex items-center justify-center bg-blue-500 py-2 px-5 rounded-lg text-white gap-2" @click="showNewForm = true">ثبت کلید <i class="fa-solid fa-add"/> </button>
+      </div>
+    </header>
+    <main>
+      <dashboard-settings-new-users v-if="showNewForm" @close="showNewForm = !showNewForm"/>
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
