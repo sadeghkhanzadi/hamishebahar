4f07a27303b2cad49eaf1e99c044f3633b09c149
Eisa <esakhanzadi@gmail.com>
2024-10-12 17:53:16 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT: create component for text Editor
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ components/dashboard/textEditor/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <ckeditor :editor="editor" :data="content" @change="onChange" />
+</template>
+
+<script>
+import { ref } from 'vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {
+  // ... سایر پلاگین‌ها
+} from '@ckeditor/ckeditor5-build-classic';
+
+export default {
+  setup() {
+    const content = ref('');
+    const editor = ref(null);
+
+    const onChange = (event, editor) => {
+      content.value = editor.getData();
+    };
+
+    const createEditor = async () => {
+      editor.value = await ClassicEditor.create(document.querySelector('.editor'), {
+        plugins: [
+          // ... آرایه‌ای از پلاگین‌های مورد نظر
+        ],
+        toolbar: [
+          'heading', '|',
+          'bold', 'italic', 'underline', 'strikethrough', '|',
+          'link', 'image', 'table', 'code', '|',
+          'bulletedList', 'numberedList', '|',
+          'indent', 'blockQuote', '|',
+          'fontfamily', 'fontsize', 'fontcolor', '|',
+          'alignment', '|',
+          'undo', 'redo'
+        ]
+      });
+    };
+
+    createEditor();
+
+    return {
+      content,
+      editor,
+      onChange
+    };
+  }
+};
+</script>
