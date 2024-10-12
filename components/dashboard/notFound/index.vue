b8d98ea6e4b26b3d1bcd9373d194b1c7a71c7e3c
Eisa <esakhanzadi@gmail.com>
2024-10-12 17:52:54 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT: create component for when nothing data
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

---
+++ components/dashboard/notFound/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const {data} = defineProps({
+  data:{
+    default:`داده ای`,
+    type:String
+  }
+})
+</script>
+
+<template>
+  <div class="alert bg-red-400 rounded px-3 py-3 md:py-5 text-white text-center shadow-lg">
+    <h5 class="text-sm md:text-lg"> هیچ {{data}} یافت نشد !!! </h5>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
