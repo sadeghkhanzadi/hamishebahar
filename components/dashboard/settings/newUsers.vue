a9831b6e72b9cd2ed8f223addc8d95beedc1c9a1
Eisa <esakhanzadi@gmail.com>
2024-11-13 23:31:07 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT:modify new user
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/dashboard/settings/newUsers.vue
+++ components/dashboard/settings/newUsers.vue
@@ -1,81 +1,77 @@
 <script setup lang="ts">
-const {event, get, edit, create, pending } = defineProps(['event', 'get', 'edit', 'create', 'pending'])
+const {event, get, edit, create, pending} = defineProps(['event', 'get', 'edit', 'create', 'pending'])
 const apiBaseUrl = useState('apiBaseUrl').value
 const cookie = useCookie('jwt')
 const showPassword = ref(false)
 const emit = defineEmits(['close', 'sendData'])
 const status = ref(true)
+const image = ref(null)
+const showImage = ref(null)
 const passwordInput = ref(null)
-const form = reactive({
+const form = reactive<object>({
   name: "",
-  age: "",
-  is_active: true,
-  nationalCode:"",
-  phoneNumber:"",
-  accessPoints:""
+  enabled: true,
+  nationalCode: "",
+  phoneNumber: "",
+  password: "",
+  email: "",
+  roles: [{id: 2}],
+  picture: null
 })
 const formError = reactive({
   name: false,
-  nationalCode:false,
-  phoneNumber:false
+  nationalCode: false,
+  phoneNumber: false,
+  email: false,
+  password: false,
+  image: false,
 })
 
 
 const handleAddNews = () => {
-  console.log(form)
-  if (!form.name.length) {
+
+  if (form.name.length < 2) {
     formError.name = true
   }
-  if (!form.nationalCode.length) {
+  if (form.nationalCode.length !== 10) {
     formError.nationalCode = true
   }
-  if (!form.phoneNumber.length) {
+  if (form.phoneNumber.length !== 11 && form.phoneNumber[0] !=0 && form.phoneNumber[1] !=9) {
     formError.phoneNumber = true
+  }
+  if (form.password.length < 6) {
+    formError.password = true
+  }
+  if (form.email.length < 12) {
+    formError.email = true
   }
 
   if (
-      form.name.length &&
-      form.nationalCode.length &&
-      form.phoneNumber
+      form.name.length >= 2 &&
+      form.nationalCode.length === 10 &&
+      form.password.length >= 6 &&
+      form.email.length > 12 &&
+      form.phoneNumber.length === 11&&form.phoneNumber[0] ==0 && form.phoneNumber[1] == 9
   ) {
-    if (create) { /*  با توجه به اینکه پراپس از نوع edit یا create درخواست فرث میکنه */
-      emit('sendData', form, 'create')
-    }
-    if (edit){
-      emit('sendData', form, 'edit')
-    }
+      emit('sendData', form)
   }
 }
 
 
-/*  برای تعییر هر خبر یک پراپس از والد که ای دی فرزند است را دریافت کرده و سپس درخواست میزنیم اما با استفاده از یک دیتا مشخص میکنیم که در خروجی دیتای ما از نوع ادیت است یا create*/
-if (get) {
-  const {data, error, status} = await useFetch(() => `${apiBaseUrl}/api/v1/teacher/`, {
-    query: {
-      id: get
-    },
-    headers: {
-      Authorization: `${cookie.value}`
-    }
-  })
-  if (error.value) {
-    console.log(error.value)
-  }
-  if (status.value === 'success') {
-    console.log(data.value.result)
-    form.name = data.value.result.firstName
-    form.is_active = data.value.result.is_active
-    form.phoneNumber = data.value.result.phoneNumber
-    form.nationalCode = data.value.result.nationalCode
-    // data.value.result.documentFiles.forEach(item => {
-    //   form.medias.push({id: item.id, name: item.name})
-    // })
-  }
-
-}
-function showPasswordKey(){
+function showPasswordKey() {
   showPassword.value = !showPassword.value
   showPassword.value ? passwordInput.value.setAttribute('type', 'text') : passwordInput.value.setAttribute('type', 'password')
+}
+function addImage(){
+  const obj = {
+    lastModified:image.value.files[0].lastModified,
+    name:image.value.files[0].name,
+    size:image.value.files[0].size,
+    type:image.value.files[0].type,
+    lastModifiedDate:image.value.files[0].lastModifiedDate,
+  }
+  form.picture = obj
+  showImage.value = URL.createObjectURL(image.value.files[0])
 }
 </script>
 
@@ -86,86 +82,69 @@ function showPasswordKey(){
         <h1>{{ event }}</h1>
       </div>
       <form @submit.prevent="handleAddNews" class="flex flex-col gap-y-3">
-        <section class="flex justify-between gap-2">
-          <section class="input-group w-full flex flex-col gap-2">
+        <div class="flex  justify-start gap-2 items-center mb-2">
+          <form action="">
+            <label for="image" class="px-3 py-1.5 border border-black  text-sm hover:bg-black hover:text-white rounded transition duration-200 ease-linear cursor-pointer ">
+              انتخاب عکس
+              <input @change="addImage" ref="image" type="file" id="image" class="hidden" pattern="jpg , png , webp">
+            </label>
+          </form>
+          <div v-if="showImage">
+            <img class="w-16 h-16 text-xs text-center rounded-full border border-gray-300 p-0.5 object-cover" :src="showImage" alt="image profile">
+          </div>
+          <span v-if="formError.image && form.picture != null" class="error">لطفا یک عکس برای پروفایل کاربر انتخاب کنید</span>
+        </div>
+        <div class="flex justify-between items-baseline gap-2">
+          <div class="input-group w-full flex flex-col gap-2">
             <label for="title">نام :</label>
-            <input type="text" v-model.trim="form.name" id="title">
-            <p v-if="formError.name" class="error">لطفا نام را وارد کنید .</p>
-          </section>
-          <section class="input-group w-full flex flex-col gap-2">
+            <input type="text" v-model.trim="form.name" id="title" class="placeholder:text-xs" placeholder="نام" @keydown="formError.name = false">
+            <span v-if="formError.name" class="error">لطفا نام را وارد کنید .</span>
+          </div>
+          <div class="input-group w-full flex flex-col gap-2">
             <label for="text">کد ملی :</label>
-            <input v-model.trim="form.nationalCode"/>
-            <p v-if="formError.nationalCode" class="error">لطفا کدملی را وارد کنید</p>
-          </section>
-        </section>
-        <section class="flex justify-between gap-2">
-          <section class="input-group w-full flex flex-col gap-2">
+            <input v-model.trim="form.nationalCode" class="placeholder:text-xs" placeholder="کدملی" @keydown="formError.nationalCode =false"/>
+            <span v-if="formError.nationalCode" class="error">کدملی باید 10 رقمی باشد .</span>
+          </div>
+        </div>
+        <div class="flex justify-between items-baseline gap-2">
+          <div class="input-group w-full flex flex-col gap-2">
             <label for="text">شماره تماس:</label>
-            <input v-model.trim="form.phoneNumber"/>
-            <p v-if="formError.phoneNumber" class="error">لطفا شماره تماس را وارد کنید</p>
-          </section>
-          <section class="input-group w-full flex flex-col gap-2">
-            <label for="text">کلمه عبور :</label>
+            <input v-model.trim="form.phoneNumber" class="placeholder:text-xs" placeholder="نمونه: 09111234567" @keydown="formError.phoneNumber = false"/>
+            <span v-if="formError.phoneNumber" class="error">لطفا شماره تماس را وارد کنید</span>
+          </div>
+          <div class="input-group w-full flex flex-col gap-2">
+            <label for="text">رمز عبور :</label>
             <div class="relative bg-amber-400">
-              <input ref="passwordInput" type="password" v-model.trim="form.password"/>
+              <input ref="passwordInput" type="password" v-model.trim="form.password" class="placeholder:text-xs" placeholder="رمز عبور" @keydown="formError.password = false"/>
               <span class="absolute  left-1 transform translate-y-1/2 translate-x-1/2 " @click="showPasswordKey">
                 <i v-if="showPassword" class="fa-solid fa-eye-slash"/>
                 <i v-else class="fa-solid fa-eye"/>
               </span>
             </div>
-            <p v-if="formError.password" class="error">لطفا کلمه عبور را وارد کنید</p>
-          </section>
-        </section>
-        <section class="flex justify-between gap-2">
-          <section class="input-group w-full flex flex-col gap-2">
-            <label for="text">ایمیل :</label>
-            <input type="email" v-model.trim="form.email"/>
-            <p v-if="formError.email" class="error">لطفا ایمیل را وارد کنید</p>
-          </section>
-        </section>
+            <span v-if="formError.password" class="error"> رمز عبور باید حداقل 6 کارکتر باشد. </span>
+          </div>
+        </div>
+        <div class="input-group w-full flex flex-col gap-2">
+          <label for="text">ایمیل :</label>
+          <input type="email" v-model.trim="form.email" class="placeholder:text-xs" placeholder="ایمیل" @keydown="formError.email = false"/>
+          <span v-if="formError.email" class="error">ایمیل باید حداقل 13 کارکتر و شامل @ باشد</span>
+        </div>
         <div class="flex flex-col gap-1.5">
           <label for="accessPoints" class="">سطح دسترسی :</label>
-          <select id="accessPoints" v-model="form.accessPoints" class="flex w-full border-gray-300 py-1 px-2 text-sm rounded  border">
-            <option value="user">کاربر</option>
-            <option value="admin">ادمین</option>
+          <select id="accessPoints" v-model="form.roles"
+                  class="flex w-full border-gray-300 py-1 px-2 text-sm rounded  border">
+            <option :value="[{id:2}]">کاربر</option>
+            <option :value="[{id:1} , {id:2}]">مدیر</option>
           </select>
         </div>
-        <section class="input-group w-full flex flex-col gap-2" v-if="false">
-          <h5 >سطح دسترسی :</h5>
-          <div class="flex items-center gap-2">
-            <input value="OP_ACCESS_ADMIN" type="checkbox" class="w-4 h-4" v-model="form.accessLevel" id="status">
-            <label> دسترسی به صفحه  و منو ها ادمین سایت</label>
-          </div>
-          <div class="flex items-center gap-2">
-            <input value="OP_ADMIN_EDIT_USER" type="checkbox" class="w-4 h-4" v-model="form.accessLevel" id="status">
-            <label> دسترسی اینکه بتوان همه کاربر ها را ویرایش کرد</label>
-          </div>
-          <div class="flex items-center gap-2">
-            <input value="OP_ADMIN_DELETE_USER" type="checkbox" class="w-4 h-4" v-model="form.accessLevel" id="status">
-            <label> دسترسی اینکه بتوان هر کاربری را حذف کرد</label>
-          </div>
-          <div class="flex items-center gap-2">
-            <input value="OP_EDIT_USER" type="checkbox" class="w-4 h-4" v-model="form.accessLevel" id="status">
-            <label> دسترسی اینکه هر کاربر بتواند فقط خودش را ویرایش کند</label>
-          </div>
-          <div class="flex items-center gap-2">
-            <input value="OP_ACCESS_USER" type="checkbox" class="w-4 h-4" v-model="form.accessLevel" id="status">
-            <label> دسترسی به صفحه  و منو های مربوط به کاربر های عادی سایت (مادر و پدر ها  میتونن کیف پولشون و هزینه ها و نمرات و ... رو ببینن)</label>
-          </div>
-          <span v-if="formError.accessLevel">حداقل یک مورد را برای تعیین سطح دسترسی باید انتخاب کنید .</span>
-        </section>
-
-
-
-
-        <section class="input-group w-full flex flex-col gap-2">
+        <div class="input-group w-full flex flex-col gap-2">
           <label for="status">وضعیت :</label>
           <div class="flex items-center gap-2">
-            <input type="checkbox" class="w-4 h-4" v-model="form.is_active" id="status">
+            <input type="checkbox" class="w-4 h-4" v-model="form.enabled" id="status" >
             <span v-if="status"> فعال </span>
             <span v-else> غیر فعال </span>
           </div>
-        </section>
+        </div>
 
         <div class="input-group">
           <button class="w-full text-center bg-red-500 py-2 rounded text-white" :disabled="pending">ثبت</button>
@@ -186,12 +165,13 @@ label {
   font-weight: var(--font-bold);
 }
 
-.input-group{
-  h5{
+.input-group {
+  h5 {
     font-size: var(--title-md);
     font-weight: var(--font-bold);
   }
-  div  label{
+
+  div label {
     font-size: var(--title-md);
     font-weight: var(--font-regular);
   }
@@ -236,5 +216,6 @@ button {
 .error {
   font-size: var(--text-sm);
   color: #ee4a62;
+  padding-right: 5px ;
 }
 </style>
