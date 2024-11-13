74357b067d2260611b86faecd70493193d963404
Eisa <esakhanzadi@gmail.com>
2024-11-13 21:59:23 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT: connect to api
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- components/home/main/card/education.vue
+++ components/home/main/card/education.vue
@@ -1,35 +1,49 @@
 <script setup lang="ts">
-const props = defineProps(['data'])
+import {useFaqStore} from "~/store/front/faq";
+
 const collapses = ref(null)
+const icons = ref(null)
+const storeFaq = useFaqStore()
+const faqData = computed(() => storeFaq.getFaq)
+const colors = [
+  "#8E56FF",
+  "#F8941F",
+  "#1AB69D"
+]
+storeFaq.fetchingFAQ()
 function show(index) {
-  if (collapses.value[index].style.maxHeight){
+  if (collapses.value[index].style.maxHeight) {
     collapses.value[index].style.maxHeight = null
-  }else{
+  } else {
     removeCollapseShow()
     collapses.value[index].style.maxHeight = `${collapses.value[index].scrollHeight}px`
-    collapses.value[index].style.maxHeight.lastElementChild.transform=`rotate(180deg)`
+    icons.value[index].classList.add('rotate-angle')
   }
 }
-function removeCollapseShow(){
-  collapses.value.forEach((item)=>{
+
+function removeCollapseShow() {
+  icons.value.forEach(item => item.classList.remove('rotate-angle'))
+  collapses.value.forEach((item) => {
     item.style.maxHeight = null
   })
 }
+
+
+console.log(faqData.value)
 </script>
 
 <template>
-  <div class="accordion-collapse education-card flex flex-col gap-5" data-aos="zoom-in"  >
-    <div class="collapse-container "  v-for="(item,index) in props.data" :key="index">
-      <div class="button text-red-600 flex justify-between px-5 py-4" :style="`background:${item.color}`"
+  {
+  <div v-if="faqData && faqData.length" class="accordion-collapse education-card flex flex-col gap-5" data-aos="zoom-in">
+    <div class="collapse-container " v-for="(item,index) in faqData" :key="index">
+      <div class="button text-red-600 flex justify-between px-5 py-4 cursor-pointer"
+           :style="`background:${colors[index]}`"
            @click="show(index)">
         <h3>{{ item.title }}</h3>
-        <span><i class="fa-solid fa-angle-down transition duration-300 ease-linear"></i></span>
+        <span><i ref="icons" class="fa-solid fa-angle-down transition duration-300 ease-linear"></i></span>
       </div>
       <div class="text  " ref="collapses">
         <p class="p-3">
-          {{ item.text }}
-          {{ item.text }}
-          {{ item.text }}
           {{ item.text }}
         </p>
       </div>
@@ -46,7 +60,8 @@ function removeCollapseShow(){
 .text {
   overflow: hidden;
   transition: 0.5s;
-  max-height:0;
+  max-height: 0;
+
   p {
     background-color: #FFFFFF;
   }
@@ -61,5 +76,13 @@ function removeCollapseShow(){
   display: none;
 }
 
+i {
+  transition: 0.2s ease;
+}
+
+.rotate-angle {
+  transform: rotate(180deg);
+
+}
 
 </style>
