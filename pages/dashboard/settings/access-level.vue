c08ac5e4dae4070183a0f22ae5d8e7b93ba27294
Eisa <esakhanzadi@gmail.com>
2024-11-13 21:54:53 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT: create access-level page in user dashboard
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- pages/dashboard/settings/access-level.vue
+++ pages/dashboard/settings/access-level.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import {useDashboardAccessLevelStore} from "~/store/dashboard/accessLevel";
+
 const showNewForm = ref(true)
+const store = useDashboardAccessLevelStore()
 const breadcrumb = [
   {
     name:"سطوح دسترسی"
   }
 ]
+function register(obj){
+store.register(obj)
+}
 </script>
 
 <template>
@@ -14,16 +20,31 @@ const breadcrumb = [
         <dashboard-breadcrumb :data="breadcrumb"/>
       </div>
       <div class="flex gap-5">
-        <button class="remove-key flex items-center justify-center bg-red-500 py-2 px-5 rounded-lg text-white gap-2" @click="">حذف کلید  <i class="fa-solid fa-trash-alt"/></button>
-        <button class="write-key flex items-center justify-center bg-blue-500 py-2 px-5 rounded-lg text-white gap-2" @click="showNewForm = true">ثبت کلید <i class="fa-solid fa-add"/> </button>
+        <button class="write-key flex items-center justify-center bg-blue-500 py-2 px-5 rounded-md text-white gap-2 text-sm" @click="showNewForm = true">افزودن کاربر <i class="fa-solid fa-add"/> </button>
       </div>
     </header>
     <main>
-      <dashboard-settings-new-users v-if="showNewForm" @close="showNewForm = !showNewForm"/>
+      <transition name="showModal">
+      <dashboard-settings-new-users v-if="showNewForm" @close="showNewForm = !showNewForm" @sendData="register"/>
+      </transition>
     </main>
   </div>
 </template>
 
 <style scoped lang="scss">
+.showModal-leave-active {
+ animation: show-modal 0.35s ease  forwards reverse ;
+}
+.showModal-enter-active{
+   animation: show-modal 0.35s ease  forwards ;
+}
 
+  @keyframes show-modal {
+    from{
+      opacity: 0;
+    }
+    to{
+      opacity: 1;
+    }
+  }
 </style>
