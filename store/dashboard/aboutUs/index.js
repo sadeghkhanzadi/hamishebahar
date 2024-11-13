d7a4251f0d3db17afa9633da6c7333b905fec8a3
Eisa <esakhanzadi@gmail.com>
2024-11-13 22:09:42 +0330

    USER_STORY: 10 TASK_NUMBER: 10 BUG_NUMBER: 0 COMMENT: add store for aboutus page in userDashboard
    --COMMIT_TYPE NEW
    --FIX_BUG: 0.0.0

--- store/dashboard/aboutUs/index.js
+++ store/dashboard/aboutUs/index.js
@@ -6,33 +6,72 @@ export const useDashboardAboutUsStore = defineStore('useDashboardAboutUsStore', 
             apiBaseUrl: useRuntimeConfig().public.apiBaseUrl,
             cookie: useCookie('jwt'),
             aboutUs: "",
-
         }
     },
     getters: {
-        getPlans(state) {
-            return state.plans;
+        getAboutUs(state) {
+            return state.aboutUs;
         }
     },
-    actions:{
-        async fetchingAboutUs(){
-            const {data , status , error} = await useAsyncData( 'fetchingAboutUs' ,()=>$fetch(`${this.apiBaseUrl}/api/v1/view/aboutus`))
-            if (status.value ==='success'){
-                this.plans = data.value.result
-                console.log(this.plans)
+    actions: {
+        async fetchingAboutUs() {
+            const {
+                data,
+                status,
+                error
+            } = await useAsyncData('fetchingAboutUs', () => $fetch(`${this.apiBaseUrl}/api/v1/view/aboutus`))
+            if (status.value === 'success') {
+                this.aboutUs = data.value.result
+                console.log(this.aboutUs)
             }
-            if(status.value === 'fail' || error.value){
+            if (status.value === 'fail' || error.value) {
                 console.log(error.value)
             }
         },
-        async EditAboutUs(obj){
-            const {data , status , error} = await useAsyncData( 'EditAboutUs' ,()=>$fetch(`${this.apiBaseUrl}/api/v1/aboutus/${obj.id}`))
-            if (status.value ==='success'){
-                console.log(this.plans)
-             await this.fetchingAboutUs()
+        async insertAboutUs(obj) {
+            const {
+                data,
+                status,
+                error
+            } = await useAsyncData('insertAboutUs', () => $fetch(`${this.apiBaseUrl}/api/v1/insert/aboutus`, {
+                method: "POST",
+                body: JSON.stringify(obj),
+                headers: {
+                    Authorization: `${this.cookie}`,
+                }
+            }))
+            if (status.value === 'success') {
+                console.log(this.aboutUs)
+                await this.fetchingAboutUs()
+                useNuxtApp().$toast.success('درخواست شما با موفقیت ثبت شد.')
             }
-            if(status.value === 'fail' || error.value){
+            if (status.value === 'fail' || error.value) {
                 console.log(error.value)
+                useNuxtApp().$toast.warning('دروخواست شما با خطا مواجه شد لطفا مجدد امتحان کنید .')
+            }
+        },
+        async EditAboutUs(obj) {
+            const {
+                data,
+                status,
+                error
+            } = await useAsyncData('EditAboutUs', () => $fetch(`${this.apiBaseUrl}/api/v1/aboutus/${obj.id}`, {
+                method: "PUT",
+                body: JSON.stringify(obj),
+                headers: {
+                    Authorization: `${this.cookie}`,
+                }
+            }))
+            if (status.value === 'success') {
+                console.log(this.aboutUs)
+                await this.fetchingAboutUs()
+                useNuxtApp().$toast.success('درخواست شما با موفقیت ثبت شد.')
+
+            }
+            if (status.value === 'fail' || error.value) {
+                console.log(error.value)
+                useNuxtApp().$toast.warning('دروخواست شما با خطا مواجه شد لطفا مجدد امتحان کنید .')
+
             }
         },
 
